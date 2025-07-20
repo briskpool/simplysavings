c9b8a85b79e81545898d37c0299eeb3241987bc7
briskpool <muzammil.mykhan@gmail.com>
2025-07-20 14:38:54 +0500

    updates in emails

--- templates/contact_mail.php
+++ templates/contact_mail.php
@@ -5,32 +5,31 @@
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
     <title>Contact</title>
     <meta name="description" content="New Account Email Template.">
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700&amp;family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,900;1,800&amp;display=swap" rel="stylesheet">
     <style type="text/css">
     a:hover {
         text-decoration: none !important;
     }
     a:link{
-        color:#ffaf32;
+        color:#167A4E;
     }
     </style>
 </head>
 
-<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #FAF3E7;" leftmargin="0">
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #D5EFE3;" leftmargin="0">
     <!-- 100% body table -->
-    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#FAF3E7"
-        style="@import url(https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700&amp;family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,900;1,800&amp;display=swap); font-family: 'Nunito Sans', sans-serif;">
+    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#D5EFE3"
+        style="@import url(https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap); font-family: 'DM Sans', sans-serif;">
         <tr>
             <td>
-                <table style="background-color: #FAF3E7; max-width:670px; margin:0 auto;" width="100%" border="0"
+                <table style="background-color: #D5EFE3; max-width:670px; margin:0 auto;" width="100%" border="0"
                     align="center" cellpadding="0" cellspacing="0">
                     <tr>
                         <td style="height:40px;">&nbsp;</td>
                     </tr>
                     <tr>
                         <td style="text-align:center;">
-                            <a href="https://al-tabbaahackettcapital.com/" title="logo" target="_blank">
-                                <img width="170" src="https://al-tabbaahackettcapital.com//templates/email-logo.png" title="logo"
+                            <a href="https://simplysavings.com" title="logo" target="_blank">
+                                <img width="170" src="https://simplysavings.com/templates/email-logo.png" title="logo"
                                     alt="logo">
                             </a>
                         </td>
@@ -48,13 +47,13 @@
                                 <tr>
                                     <td style="padding:0 35px;">
                                         <h1
-                                            style="color:#1a1a1a; font-weight:500; margin:0;font-size:26px;font-family:'Rubik',sans-serif;">
+                                            style="color:#1a1a1a; font-weight:500; margin:0;font-size:26px;font-family:'DM Sans',sans-serif;">
                                             * New Message
                                         </h1>
                                         <p style="font-size:15px; color:#455056; margin:8px 0 0; line-height:24px;">
                                             Received from Contact Form</p>
                                         <span
-                                            style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #ffaf32; width:100px;"></span>
+                                            style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #167A4E; width:100px;"></span>
                                         <p
                                             style="color:#455056; font-size:18px;line-height:20px; margin:0; font-weight: 500; text-align:left;">
                                             <strong
@@ -81,7 +80,7 @@
                         <td style="text-align:center;">
                             <p
                                 style="font-size:14px; color:rgba(69, 80, 86, 0.7411764705882353); line-height:18px; margin:0 0 0;">
-                                &copy; <strong>www.al-tabbaahackettcapital.com</strong> </p>
+                                &copy; <strong>www.simplysavings.com</strong> </p>
                         </td>
                     </tr>
                     <tr>
