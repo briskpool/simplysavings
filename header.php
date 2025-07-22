66082ddbc63069e1bf4861b80f066118a060a2a7
briskpool <muzammil.mykhan@gmail.com>
2025-07-22 23:15:22 +0500

    Update meta tags and sitemap for Simply Savings
    Added and updated Open Graph meta tags in header.php for improved SEO and social sharing. Updated sitemap.xml URLs to use simplesavings.com instead of al-tabbaahackettcapital.com.

--- header.php
+++ header.php
@@ -11,9 +11,13 @@
         content="Savings, Investment, Simply Savings, Ways To Invest, Simple Savings, Simple Growth, Simple Income">
     <meta name="author" content="Simply Savings">
     <!--  Essential META Tags -->
+    <meta name="title" property="og:title" content="Simply Savings">
+    <meta name="description" property="og:description"
+        content="Make Your Savings Work For You. There are many ways to invest your money, and at Simply Savings we offer a wide range of options to suit your needs.">
     <meta property="og:title" content="Simply Savings">
     <meta property="og:type"
         content="There are many ways to invest your money, and at Simply Savings we offer a wide range of options to suit your needs." />
+    <meta name="image" property="og:image" content="http://simplysavings.com//images/website-preview.png">
     <meta property="og:image" content="http://simplysavings.com//images/website-preview.png">
     <meta property="og:url" content="http://simplysavings.com/index.php">
     <meta name="twitter:card" content="summary_large_image">
@@ -35,7 +39,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
+        rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
     <!-- Google tag (gtag.js) -->
@@ -55,5 +61,5 @@
 
 <body>
     <div id="preloader">
-  <img src="images/logos/logo.svg" alt="Logo" class="logo" />
-</div>
+        <img src="images/logos/logo.svg" alt="Logo" class="logo" />
+    </div>
